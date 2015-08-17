72d46d3cde25e510589738f7a5135fcc1c25311c
Nicholas Chin <nchin.nc@gmail.com>
2015-08-17 16:19:17 +0100

    created separate header and footer files. Added about and contact stub pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
