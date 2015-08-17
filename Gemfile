d74f8f056048882540522856ba9376923e17ff47
Nicholas Chin <nchin.nc@gmail.com>
2015-08-17 11:09:09 +0100

    test out an embedded js in html file

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.3'
 # Use postgresql as the database for Active Record
 gem 'pg'
+
+group :development do
+  gem "better_errors"
+end
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/assets/javascripts/welcome-jquery.js
+++ app/assets/javascripts/welcome-jquery.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+  
+
   $('p').append('<p class = "list">This is an extra comment</p>' +
-   '<p class = "list">Click to remove comments!</p>');
+   '<p class = "list">Click to remove comments!</p>'+
+   '<p class = "list">Hello Darshan</p>' +
+   '<p class = "list">Hello Izabela, how are you today? I like trains!</p>');
+  
+
   $('p.list').click(function(){
     $(this).remove();
   });
@@ -8,5 +14,5 @@ $(document).ready(function(){
 
   $('.menu-link').bigSlide();
 
-  
-})
+
+});
